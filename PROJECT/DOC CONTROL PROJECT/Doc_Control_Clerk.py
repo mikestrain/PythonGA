d12759eb8feba1362160da8b3b26fa8d5f8f9858
Mike Strain <michael.e.strain@gmail.com>
2020-02-09 14:28:10 -0500

    added test mode, some input lines

--- PROJECT/DOC CONTROL PROJECT/Doc_Control_Clerk.py
+++ PROJECT/DOC CONTROL PROJECT/Doc_Control_Clerk.py
@@ -3,7 +3,7 @@
 import os
 import shutil
 os.system("clear")
-print("\n\n\n\n\n\n")
+testing_mode = True
 
 pDropbox = './1 DOCUMENT DROPBOX'
 pQueue = './2 - DOC CONTROL QUEUE'
@@ -14,31 +14,72 @@ Q_full_names = os.listdir(pQueue)
 A_folders = os.listdir(pActive)
 O_folders = os.listdir(pObsolete)
 
+print("""
+WELCOME TO THE BUSEK DOC CONTROL CLERK PROGRAM.
+Written by Mike Strain on 2/2/2020.
+
+ - Meant to check file name formatting, and move files around to appropriate folders.
+ - Capitalizes files if they are in lowercase
+ 
+""")
+input('Press any key to continue...\n')
+
 #%% Setup document names and check for bad part numbers
+print("Checking all document names for bad formatting or bad part numbers.")
+print("All documents should follow the format 700XXXX (X) DESCRIPTION, PART, ETC\n")
 Q_doc_names = []
 Q_doc_numbers = []
 Q_doc_extensions = []
+Q_doc_revs = []
 
 for name in Q_full_names:
 
     # CHECK LOGIC FOR DOCUMENT NUMBER - IS IT 8 DIGITS? ARE THEY ALL NUMBERS?
     doc_number = name[0:8]
     doc_extension = name.split('.')[-1]
+
+    # check that it's an integer
     try: int(doc_number)
     except: 
-        print("This part {0} isn't named correctly!".format(doc_number))
+        print("\tThe part {0} isn't an integer. Please reformat.".format(doc_number))
         continue
 
+    # check that the number doesn't contain a space
     if ' ' in doc_number:
-        print("This part number {0} contains a space, or isn't 8 digits.".format(doc_number))
+        print("\tThe part number {0} contains a space, or isn't 8 digits. Please reformat.".format(doc_number))
         continue
 
+    doc_split = name.split()
+
+    # check that the file name is not a monolithic string
+    try: doc_split[1]
+    except:
+        print("\tThe part {0} is one single string. Please reformat.".format(doc_number))
+        continue
+
+    # check that the second entry in doc_split contains open/closed parenthesis
+    if ('(' or ')') not in doc_split[1]:
+        print("\tThe part {0}'s revision is not enclosed by (). Please reformat.".format(doc_number))
+        continue
+
+    # check that the part says (1) not (REV 1)
+    if 'R' in doc_split[1]:
+        print('\tPlease reformat {0} as (#) instead of (REV #) or (R#).'.format(doc_number))
+        continue
+    
+    doc_rev = doc_split[1][1:-1] #meant to remove the parenthesis from the revision
+    print(doc_rev,'!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
     Q_doc_numbers.append(doc_number)
     Q_doc_names.append(name)
     Q_doc_extensions.append(doc_extension)
+    Q_doc_revs.append(doc_rev)
+
+print('\nAll document names checked for formatting...')
+input('Press any key to continue...')
 
 #%% Check unique document numbers
-
+os.system('clear')
+print("Checking documents for unique part numbers.")
 Q_doc_numbers_unique = []
 for number in Q_doc_numbers:
 
@@ -47,11 +88,14 @@ for number in Q_doc_numbers:
         Q_doc_numbers_unique.append(number)
 
 
-print(Q_doc_numbers_unique)
-print(len(Q_doc_numbers_unique))
+# print(Q_doc_numbers_unique)
+print('The Queue contains',len(Q_doc_numbers_unique),'unique part numbers to be processed.')
+print("Finished checking documents for unique part numbers.\n\n")
+input('Press any key to continue...')
 
 # %%
-
+os.system('clear')
+print("Forming a Queue from unique part numbers, with dictionary of properties...")
 Queue = {}
 for u_number in Q_doc_numbers_unique:
     
@@ -64,12 +108,18 @@ for u_number in Q_doc_numbers_unique:
             fileInfo.append({
                 'number' : Q_doc_numbers[i],
                 'name' : Q_doc_names[i],
+                'revision' : Q_doc_revs[i],
                 'extension' : Q_doc_extensions[i]
             })
     
     Queue[u_number] = fileInfo
+print('Finished forming Queue.\n\n')
+input('Press any key to continue...')
 
 # %% START MOVING FILES AND CREATING FOLDERS...
+os.system('clear')
+print('Moving files and creating new folders.')
+
 
 for u_number in Queue:
 
@@ -82,7 +132,7 @@ for u_number in Queue:
     if u_number not in A_folders: # if it's a completely new part number
         
         print('\t{0} is a new number, and needs a new folder.'.format(u_number))
-        os.mkdir(pActive + '/' + u_number)
+        if not testing_mode: os.mkdir(pActive + '/' + u_number)
         print('\t***New directory created in Active files.')
 
     
@@ -98,7 +148,7 @@ for u_number in Queue:
         if u_number not in O_folders: # if it doesn't yet have an obsolete folder
 
             print('\t{0} needs an obsolete folder.'.format(u_number))
-            os.mkdir(pObsolete + '/' + u_number)
+            if not testing_mode: os.mkdir(pObsolete + '/' + u_number)
             print('\t***New directory created in Obsolete files.')
         
         # MOVE FROM ACTIVE TO OBSOLETE
@@ -106,22 +156,26 @@ for u_number in Queue:
         files_to_obsolete = os.listdir(pActive + '/' + u_number)
 
         for each_file in files_to_obsolete:
-            print("\t\tMoving {0} to Obsolete...".format(each_file))
+            print("\t\tMoving {0} from Active to Obsolete...".format(each_file))
             old_path = pActive + '/' + u_number + '/' + each_file
-            new_path = pObsolete + '/' + u_number + '/' + each_file
-            shutil.move(old_path, new_path) # <------- actually moves the file
+            new_path = pObsolete + '/' + u_number + '/' + each_file.upper()
+            if not testing_mode: shutil.move(old_path, new_path) # <------- actually moves the file
 
 
     # MOVE FROM QUEUE TO ACTIVE
     print('\tMoving {0} from the queue to active...'.format(u_number))
 
     for each_file in Queue[u_number]:
-        print("\t\tThe name of this file is {0}".format(each_file['name']))
+        print("\t\tMoving {0} from Queue to Active".format(each_file['name']))
         old_path = pQueue + '/' + each_file['name']
-        new_path = pActive + '/' + each_file['name']
-        shutil.move(old_path, new_path) # <------- actually moves the file
+        new_path = pActive + '/' + u_number + '/' + each_file['name'].upper()
+        if not testing_mode: shutil.move(old_path, new_path) # <------- actually moves the file
     
     print('\n')
+    input('Press a key to continue...')
+
+print('Finished moving files and creating new folders.\n\n')
+input('press any key to exit!')
 
 # %% 
 
@@ -129,4 +183,6 @@ for u_number in Queue:
 # add in a cross reference for project number in PDM search?
 # add in an email function
 # add in a document tracking function
+
+
 
